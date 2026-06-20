f88b0fc42c19c0906c904acd1b567fd36f597fb0
Sami Sandqvist <sami.sandqvist@paretosoftware.fi>
2026-06-20 18:14:07 +0300

    feat: add draggable split layout and React Flow graph neighborhood view

--- packages/web/src/test/setup.ts
+++ packages/web/src/test/setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom/vitest";
+
+class ResizeObserverStub {
+  observe(): void {}
+  unobserve(): void {}
+  disconnect(): void {}
+}
+globalThis.ResizeObserver = ResizeObserverStub as unknown as typeof ResizeObserver;
